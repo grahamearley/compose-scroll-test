78bd7f58623e70ffe71b3eb2eae8360633a40243
Graham Earley <gearley@squareup.com>
2022-09-29 14:16:22 -0500

    Increase height of the text rows — this makes scrolling perfectly smooth

--- app/src/main/java/com/example/composeperftest/MainActivity.kt
+++ app/src/main/java/com/example/composeperftest/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +18,11 @@ class MainActivity : ComponentActivity() {
     setContent {
       LazyColumn(modifier = Modifier.fillMaxHeight().fillMaxWidth()) {
         items(1000) { number ->
-          BasicText(text = "Text $number", modifier = Modifier.fillMaxWidth())
+          BasicText(
+            text = "Text $number",
+            modifier = Modifier.fillMaxWidth()
+              .height(100.dp)
+          )
         }
       }
     }
